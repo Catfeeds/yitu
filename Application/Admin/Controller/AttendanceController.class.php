a7f1e5e4405a0fcbf8e4ba9a022df9a4955b2894
cd_leo <cd_leo@163.com>
2018-12-13 14:01:05 +0800

    V1.0.0.2

---
+++ Application/Admin/Controller/AttendanceController.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/13
+ * Time: 12:20
+ */
+
+namespace Admin\Controller;
+
+
+class AttendanceController extends AdminController
+{
+
+
+    public function index(){
+
+        $this->display();
+    }
+
+    public function add(){
+        $this->display();
+    }
+}
